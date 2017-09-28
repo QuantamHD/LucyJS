f24397eb3ff26e359b03ba5c07a93cb599977cc0
Evan <econrad@zagmail.gonzaga.edu>
2017-09-28 16:51:09 -0600

    Fix snips (#4)

--- test/snippets/opendir_fail.js
+++ test/snippets/opendir_fail.js
@@ -2,5 +2,7 @@
  * This code should throw and error and not fail.
  */
 
-const lucy = require("../../lib/index.js");
-const index_searcher = new lucy.IndexSearcher("./HAHAHA_I_AM_NOTHING");
+try {
+  const lucy = require("../../lib/index.js");
+  const index_searcher = new lucy.IndexSearcher("./HAHAHA_I_AM_NOTHING");
+} catch (e) {}
